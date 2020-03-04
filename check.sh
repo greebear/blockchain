7629d95df8c5b8aa6fc3ea0df85b4845cd0bc665
zhengshuangyang <greebear@qq.com>
2020-03-04 09:29:32 +0800

    测试

--- check.sh
+++ check.sh
@@ -1,5 +1,1 @@
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-CORE_PEER_ADDRESS=peer0.org2.example.com:9051
-CORE_PEER_LOCALMSPID="Org2MSP"
-CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-peer channel join -b mychannel.block
+测试
